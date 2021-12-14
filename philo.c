57feb53378b7ef3b85963d5d71b4d062403ad9f5
Jeremy Long <jlong@shi-r8-p3.s19.be>
2021-12-14 14:58:26 +0100

    mardi

--- philo.c
+++ philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlong <jlong@student.s19.be>               +#+  +:+       +#+        */
+/*   By: jlong <jlong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 17:53:08 by jlong             #+#    #+#             */
-/*   Updated: 2021/12/13 19:06:43 by jlong            ###   ########.fr       */
+/*   Updated: 2021/12/14 14:57:48 by jlong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,70 @@ void	ft_error()
 {
     printf("Number of arguments is not correct !\n");
 }
-int	get_data(int argc, char **av, t_data *data)
+
+t_data  *get_data(int argc, char **av)
 {
-    &data->number_of_philo = ft_atoi(av[1]);
-    &data->time_to_die = ft_atoi(av[2]);
-    &data->time_to_eat = ft_atoi(av[3]);
-    &data->time_to_sleep = ft_atoi(av[4]);
-    &data->number_eat = ft_atoi(av[5]);
-    return (1);
+    t_data *data;
+
+    data = malloc(sizeof(t_data));
+    if (!data)
+        return (NULL);
+    if (argc == 5 || argc == 6)
+    {
+        data->number_of_philo = ft_atoi(av[1]);
+        data->time_to_die = ft_atoi(av[2]);
+        data->time_to_eat = ft_atoi(av[3]);
+        data->time_to_sleep = ft_atoi(av[4]);
+        if (argc == 6)
+            data->number_eat = ft_atoi(av[5]);
+        else
+            data->number_eat = 1;
+    }
+    return (data);
 }
 
-void	init_struct_philo(t_philo *philo)
+void	init_struct_philo(t_philo *philo, t_data *data)
 {
 	philo->left_fork = 0;
 	philo->right_fork = 0;
-	philo->philo = 0;
+	philo->philo_id = 0;
+    philo->nbr_eat = data->number_eat;
+    philo->data = data;
 }
+
+void    routine_eat(t_philo *philo)
+{
+    //Faire le mutex fourchette gauche
+    //faire le mutex fourchette droite
+    //Quand il a les deux fourchette il peut manger
+    //Puis il dort quqnd il a fini et il libere les fourchette
+    int id;
+    
+    id = philo->philo_id;
+    pthread_mutex_lock(&(philo->data->fork[id]));
+    printf("Fourchette id = %d\n", id);
+    pthread_mutex_unlock(&(philo->data->fork[id]));
+}
+
+void    *routine(void *test_philo)
+{
+    t_philo *philo;
+    int     id;
+
+    philo = (t_philo *)test_philo;
+    id = philo->philo_id;
+    while (philo->nbr_eat)
+    {
+        routine_eat(philo);
+        pthread_mutex_lock(&(philo->eat));
+        printf("eat number == %d of philo number %d\n",philo->nbr_eat,  id);
+        //philo->nbr_eat--;
+        pthread_mutex_unlock(&(philo->eat));
+        philo->nbr_eat--;
+    }
+    return (NULL);
+}
+
 int creat_philo(t_data *data, t_philo *philo)
 {
 	int	i;
@@ -42,16 +90,16 @@ int creat_philo(t_data *data, t_philo *philo)
         return 0;
 	while (i < data->number_of_philo)
 	{
-		init_struct_philo(philo[i]);
-		philo->philo = i;
-        pthread_create(&philo[i]->thread, NULL, &routine, NULL);
+		init_struct_philo(&philo[i], data);
+		philo[i].philo_id = i;
+        pthread_create(&(philo[i].thread), NULL, routine, &(philo[i]));
+        i++;
         //verif le tread 
-		i++;
 	}
     i = 0;
     while (i < data->number_of_philo)
 	{
-        pthread_join(&philo[i]->thread, NULL);
+        pthread_join(philo[i].thread, NULL);
         //verif le tread 
 		i++;
 	}
@@ -61,13 +109,14 @@ int creat_philo(t_data *data, t_philo *philo)
 int	init_mutex(int argc, t_data *data, t_philo *philo)
 {
 	int	i;
+    (void)argc;
 
 	i = 0;
 	while (i < data->number_of_philo)
 	{
-        pthread_mutex_init(philo[i]->fork, NULL);
+        pthread_mutex_init(&data->fork[i], NULL);
         if (data->number_eat > 0)
-            pthread_mutex_init(philo[i]->eat, NULL);
+            pthread_mutex_init(&philo[i].eat, NULL);
 		i++;
 	}
 	return (1);
@@ -75,30 +124,27 @@ int	init_mutex(int argc, t_data *data, t_philo *philo)
 
 int main(int argc, char **av)
 {
-    t_data	data;
+    t_data	*data;
     t_philo	*philo;
 
     philo = NULL;
-    data.number_of_philo = 0;
-    data.time_to_die = 0;
-    data.time_to_sleep = 0;
-    data.time_to_eat = 0;
-    data.number_eat = 0;
-    if (!get_data(argc, av, &data))
+    data = NULL;
+    if (!(data = get_data(argc, av)))
         return (1);
-    philo = malloc(sizeof(t_philo) * data.number_of_philo);
+    philo = malloc(sizeof(t_philo) * data->number_of_philo);
     if (!philo)
     {
-        free(philo);
+        printf("error ici\n");
         return (1);
     }
-	if (!init_mutex(argc, &data, philo))
+	if (!init_mutex(argc, data, philo))
 	{
-		
+		printf("error dwdwdw\n");
 		return (1);
 	}
-	if (!creat_philo(&data, philo))
+	if (!creat_philo(data, philo))
 	{
+        printf("error llqlq\n");
 		//free la struct
 		return (1);
 	}
--- philo.h
+++ philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlong <jlong@student.s19.be>               +#+  +:+       +#+        */
+/*   By: jlong <jlong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/25 17:50:00 by jlong             #+#    #+#             */
-/*   Updated: 2021/12/13 18:11:28 by jlong            ###   ########.fr       */
+/*   Updated: 2021/12/14 14:40:07 by jlong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <stdlib.h>
 # include <errno.h>
 # include <assert.h> 
-# include <limit.h>
+# include <limits.h>
 
 typedef struct s_data
 {
@@ -28,20 +28,21 @@ typedef struct s_data
 	int	time_to_eat;
 	int	time_to_sleep;
 	int	number_eat;
+	pthread_mutex_t fork[250];
 }				t_data;
 
 typedef struct	s_philo
 {
-	pthread_t	left_fork;
-	int	philo;
-	pthread_t	thread;
-	pthread_t	right_fork;
-	pthread_mutex_t fork;
+	int				left_fork;
+	int				philo_id;
+	pthread_t		thread;
+	int				right_fork;
 	pthread_mutex_t	eat;
-	
+	int				nbr_eat;
+	t_data			*data;
 }				t_philo;
 
-int	main(void);
+int	main(int argc, char **av);
 int	ft_atoi(const char *s);
 
 #endif
